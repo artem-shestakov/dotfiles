1704309e41c4df166a67b177011d743b9d8911fd
Artem <52320860+artem-shestakov@users.noreply.github.com>
2024-01-17 16:44:46 +0300

    Update 10_apps.sh

--- 10_apps.sh
+++ 10_apps.sh
@@ -25,4 +25,8 @@ curl -LO https://dl.k8s.io/release/`curl -LS https://dl.k8s.io/release/stable.tx
 chmod +x ./kubectl
 sudo mv ./kubectl /usr/local/bin/kubectl
 
+# Create links
+sudo ln -s /usr/lib/go-1.21/bin/go /usr/local/bin/go
+sudo ln -s /usr/lib/go-1.21/bin/gofmt /usr/local/bin/gofmt
+
 echo "💪 Finished"
