3eef228a5f2a14e6f3ff93c20be9d050858d0099
Artem <52320860+artem-shestakov@users.noreply.github.com>
2024-01-30 09:37:47 +0300

    Update 10_apps.sh

--- 10_apps.sh
+++ 10_apps.sh
@@ -57,6 +57,12 @@ sudo apt -y install \
     qemu-kvm \
     virt-manager
 
+# Kind
+# For AMD64 / x86_64
+[ $(uname -m) = x86_64 ] && curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.20.0/kind-linux-amd64
+chmod +x ./kind
+sudo mv ./kind /usr/local/bin/kind
+
 # Kubectl
 curl -LO https://dl.k8s.io/release/`curl -LS https://dl.k8s.io/release/stable.txt`/bin/linux/amd64/kubectl
 chmod +x ./kubectl
